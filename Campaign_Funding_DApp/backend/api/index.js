eb5402dda130a68900e31756b7d454ebc4be2376
Zakir-Ansari <azakir780@gmail.com>
2024-12-01 11:27:38 +0530

    adding test api

--- Campaign_Funding_DApp/backend/api/index.js
+++ Campaign_Funding_DApp/backend/api/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) =>
+  res.send({
+    status: "Active",
+  })
+);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 

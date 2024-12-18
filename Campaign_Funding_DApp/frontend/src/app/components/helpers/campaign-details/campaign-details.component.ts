71441adfbf907685aae4dffc33c3632ea465a611
Zakir-Ansari <azakir780@gmail.com>
2024-12-18 18:32:24 +0530

    Adding ag grid to the app

--- Campaign_Funding_DApp/frontend/src/app/components/helpers/campaign-details/campaign-details.component.ts
+++ Campaign_Funding_DApp/frontend/src/app/components/helpers/campaign-details/campaign-details.component.ts
@@ -1,17 +1,22 @@
-import { AfterViewInit, Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
-import { Campaign } from '../../../models/campaign.model';
-import { ethers } from 'ethers';
-import { ToEthPipe } from '../../../pipes/to-eth.pipe';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { States } from '../../../constants/common.constant';
+import { Campaign, DonatorDonations } from '../../../models/campaign.model';
+import { ToEthPipe } from '../../../pipes/to-eth.pipe';
 import { CampaignService } from '../../../services/campaign/campaign.service';
 import { ToastService } from '../../../services/shared/toast/toast.service';
-import { States } from '../../../constants/common.constant';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import type { ColDef, GridApi, GridReadyEvent } from 'ag-grid-community'; // Column Definition Type Interface
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 @Component({
   selector: 'app-campaign-details',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ToEthPipe],
+  imports: [CommonModule, ReactiveFormsModule, ToEthPipe, AgGridAngular],
   templateUrl: './campaign-details.component.html',
   styleUrl: './campaign-details.component.scss',
 })
@@ -22,23 +27,52 @@ export class CampaignDetailsComponent implements OnInit {
   campaignService = inject(CampaignService);
   percentOfDonation = '0%';
   donationForm!: FormGroup;
+  gridApi!: GridApi<DonatorDonations>;
 
   isDonationSubmitted = false;
   STATES = States;
   donationProcessState!: States;
 
+  // Ag grid - Row Data: The data to be displayed.
+  rowData: DonatorDonations[] = [];
+  // Ag grid - Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: 'donator', flex: 2 },
+    {
+      field: 'donation',
+      cellClass: 'ag-right-aligned-cell',
+      headerClass: 'ag-right-aligned-header',
+      valueFormatter: params => new ToEthPipe().transform(params.data.donation),
+    },
+  ];
+  // Ag grid - Default Column Definition
+  defaultColDef = {
+    flex: 1,
+    minWidth: 120,
+    wrapHeaderText: true,
+    filter: true,
+    floatingFilter: true,
+  };
+
   ngOnInit(): void {
+    this.donationForm = new FormGroup({
+      amount: new FormControl('', [Validators.required, Validators.min(0)]),
+    });
+
     this.percentOfDonation = `${((this.campaign.totalDonation / Math.pow(10, 18) / this.campaign.target) * 100).toFixed(
       0
     )}%`;
 
-    this.donationForm = new FormGroup({
-      amount: new FormControl('', [Validators.required, Validators.min(0)]),
-    });
+    this.rowData = this.campaign.donatorDonations ?? [];
   }
 
   get f() {
     return this.donationForm.controls;
+  }
+
+  onGridReady(params: GridReadyEvent<DonatorDonations>) {
+    this.gridApi = params.api;
+    //this.gridApi.setGridOption('domLayout', 'autoHeight');
   }
 
   donate() {
